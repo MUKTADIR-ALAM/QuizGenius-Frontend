0769e346f912063ca8bd650018c8658bef09da02
Muktadir <muktadiralam38@gmail.com>
2025-03-25 12:27:23 +0600

    merge with muktadir

--- src/authPage/Login.jsx
+++ src/authPage/Login.jsx
@@ -1,10 +1,9 @@
-import React, { useContext, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router';
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
 import { FcGoogle } from "react-icons/fc";
-import { AuthContext } from '../provider/AuthProvider';
+import { AuthContext } from "../provider/AuthProvider";
 
 const LoginPage = () => {
-
   const { logInUser, signInWithGoogle } = useContext(AuthContext);
 
   const [errorMessage, setErrorMessage] = useState("");
@@ -14,37 +13,46 @@ const LoginPage = () => {
 
   const handleLoginSubmit = (e) => {
     e.preventDefault();
-    setErrorMessage('');
+    setErrorMessage("");
     const form = new FormData(e.target);
     const email = form.get("email");
     const password = form.get("password");
     logInUser(email, password)
-    .then((res)=>{navigate(location?.state ? location?.state:'/')})
-    .catch((err)=>{setErrorMessage(err.message)
-      toast.error(err.message);
-    })
+      .then((res) => {
+        navigate(location?.state ? location?.state : "/");
+      })
+      .catch((err) => {
+        setErrorMessage(err.message);
+        toast.error(err.message);
+      });
   };
 
-
-  const hadleGoogleLogin = () =>{
+  const hadleGoogleLogin = () => {
     signInWithGoogle()
-    .then((result) => {
-      navigate(location?.state ? location?.state:'/')
-      toast.success('Successfully login');
-    })
-    .catch((error) => {
-      setErrorMessage(err.message);
-      toast.error(err.message);
-    });
-  }
+      .then((result) => {
+        navigate(location?.state ? location?.state : "/");
+        toast.success("Successfully login");
+      })
+      .catch((error) => {
+        setErrorMessage(err.message);
+        toast.error(err.message);
+      });
+  };
 
   return (
     <div className="min-h-screen bg-white flex flex-col justify-center items-center">
       <div className="bg-black p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-white text-2xl font-bold mb-6 text-center">Login to Quiz Genious</h2>
+        <h2 className="text-white text-2xl font-bold mb-6 text-center">
+          Login to Quiz Genious
+        </h2>
         <form onSubmit={handleLoginSubmit} className="space-y-6">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-300">Email</label>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
@@ -54,7 +62,12 @@ const LoginPage = () => {
             />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-300">Password</label>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Password
+            </label>
             <input
               type="password"
               id="password"
@@ -79,19 +92,21 @@ const LoginPage = () => {
             >
               Sign In
             </button>
-            
           </div>
         </form>
         <div
-              onClick={hadleGoogleLogin}
-              className="mt-5 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <FcGoogle className='mr-2' size={20} />
-              Sign In with Google
-            </div>
+          onClick={hadleGoogleLogin}
+          className="mt-5 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+          <FcGoogle className="mr-2" size={20} />
+          Sign In with Google
+        </div>
         <div className="mt-4 text-center">
           <span className="text-sm text-gray-400">Don't have an account? </span>
-          <Link to={'/register'} className="text-sm text-white hover:text-blue-400">
+          <Link
+            to={"/register"}
+            className="text-sm text-white hover:text-blue-400"
+          >
             Sign Up
           </Link>
         </div>
