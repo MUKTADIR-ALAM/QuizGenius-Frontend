6477823fd28d9bc029e2233329cb5923877d662c
Muktadir <muktadiralam38@gmail.com>
2025-04-09 15:39:25 +0600

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { Route, Routes } from "react-router";
 import MainLayout from "./Layout/MainLayout";
 import Home from "./Pages/Home/Home";
-import LoginPage from './authPage/Login';
-import RegisterPage from './authPage/Register';
+import LoginPage from "./authPage/Login";
+import RegisterPage from "./authPage/Register";
 import Pricing from "./Pages/Pricing/Pricing";
-import AboutUs from './Pages/About us/AboutUs';
+import AboutUs from "./Pages/About us/AboutUs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout></MainLayout>}>
-        <Route index element={<Home></Home>}></Route>
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/about" element={<AboutUs />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<MainLayout></MainLayout>}>
+          <Route index element={<Home></Home>}></Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
